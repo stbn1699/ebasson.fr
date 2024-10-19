81f9c7d7e1c27511e6821523c50f05b9278d46ec
Plexi <stbnbasson@gmail.com>
2024-10-19 18:49:47 +0200

    feat: CommandCraftor

--- src/pages/CommandCraftor.tsx
+++ src/pages/CommandCraftor.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import "../styles/CommandCraftor.css";
 
 const CommandCraftor: React.FC = () => {
 	return (
-		<main>
-			<h1>CommandCraftor</h1>
-			<p>Details about the CommandCraftor project.</p>
+		<main className="commandCraftor-main">
+			<h1 className="commandCraftor-title">CommandCraftor</h1>
+			<p className="commandCraftor-description">
+				ComandCraftor est un projet que j'ai lance, a la fois pour acquerir de l'experience et pour repondre a
+				un besoin specifique dans la communaute des joueurs de Minecraft. J'ai remarque que beaucoup des
+				generateurs de commandes existants sont souvent trop complexes, surtout pour les jeunes joueurs ou ceux
+				qui ne cherchent qu'a generer des commandes basiques sans se perdre dans des options inutiles. Mon
+				objectif etait de creer un generateur simple, intuitif et accessible a tous, sans sacrifier les
+				fonctionnalites essentielles dont les joueurs ont besoin. Avec ComandCraftor, je voulais offrir un outil
+				qui se concentre uniquement sur l'essentiel, sans ajouter de couches de complexite qui risquent de
+				decourager les joueurs lambda, tout en etant assez performant pour satisfaire leurs attentes.
+			</p>
+			<div className="commandCraftor-buttons">
+				<div className="McButtons" data-testid="ButtonsJavaEdition">
+					<div className="mc-button full">
+						<div className="title">test</div>
+					</div>
+				</div>
+				<div className="McButtons" data-testid="ButtonsJavaEdition">
+					<div className="mc-button full">
+						<div className="title">test</div>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 };
