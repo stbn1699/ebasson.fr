1a16524ba1e40484777f6f0ba00f4576179c6b62
Plexi <stbnbasson@gmail.com>
2024-10-21 12:12:00 +0200

    feat: contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "../styles/Contact.css";
 
 const Contact: React.FC = () => {
 	return (
-		<main>
-			<h1>Contact</h1>
-			<p>Contact details and form.</p>
+		<main className="contact-main">
+			<h1 className="contact-title">Contact</h1>
+			<div className="contact-links">
+				<a href="https://github.com/stbn1699">GitHub</a><br/>
+				<a href="mailto:stbnbasson@gmail.com">E-mail</a><br/>
+				<a href="https://www.linkedin.com/in/esteban-basson-2ab8761a5/">LinkedIn</a><br/>
+				<a href="https://x.com/stbn_deb">Twitter / X</a><br/>
+				<a href="https://www.instagram.com/stbn__169/">Instagram</a><br/>
+				<a href="https://www.threads.net/@stbn__169">Threads</a><br/>
+				<a href="https://letterboxd.com/stbn169/">Letterboxd</a><br/>
+				<a href="https://www.youtube.com/channel/UCRy7b3lwxdaAHZL_Mbc2vQA">YouTube</a><br/>
+			</div>
 		</main>
 	);
 };
